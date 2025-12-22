2aaa3a7e3f3ced2c1bef90f62981deb17a3061c3
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-12-22 23:57:21 +0100

    last fix

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,18 +1,3 @@
-/*import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import nuoveReducer from "../reducer";
-import audioReducer from "../audioReducer";
-
-const Reducer = combineReducers({
-  nuove: nuoveReducer,
-  audioA: audioReducer,
-});
-
-const store = configureStore({
-  reducer: Reducer,
-});
-
-export default store; */
-
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import nuoveReducer from "../reducer";
 import audioReducer from "../audioReducer";
