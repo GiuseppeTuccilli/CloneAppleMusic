016c9dc83a1ae73195de9303e7cf6682054c203a
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-05-12 20:34:23 +0200

    inline style removal

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -6,24 +6,20 @@ import unoC from "../../assets/images/1c.png";
 
 const Top = () => {
   return (
-    <Container className="mt-5">
-      <div id="topSpace" style={{ height: "5em" }}></div>
+    <Container className="mt-5" id="topContainer">
+      <div id="topSpace"></div>
       <h1 className="text-white">Novità</h1>
-      <hr style={{ height: "2px" }} className="bg-white" />
+      <hr className="bg-white" />
       <Carousel controls={false} className="d-none d-lg-flex">
         <Carousel.Item>
           <div className="d-flex big-car">
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold pe-2 mb-auto flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoA}
-                className=" mx-1 flex-grow-1 "
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoA} className=" mx-1 flex-grow-1 " />
             </div>
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
@@ -31,11 +27,7 @@ const Top = () => {
                 Nuova casa della musica latina
               </p>
 
-              <img
-                src={unoB}
-                className=" mx-1 flex-grow-1"
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoB} className=" mx-1 flex-grow-1" />
             </div>
           </div>
         </Carousel.Item>
@@ -44,26 +36,18 @@ const Top = () => {
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold pe-2 mb-auto  flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoC}
-                className=" mx-1 flex-grow-1"
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoC} className=" mx-1 flex-grow-1" />
             </div>
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold  pe-2 mb-auto  flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoA}
-                className=" mx-1 flex-grow-1"
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoA} className=" mx-1 flex-grow-1" />
             </div>
           </div>
         </Carousel.Item>
@@ -74,14 +58,10 @@ const Top = () => {
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold pe-2 mb-auto flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoA}
-                className=" mx-1 flex-grow-1 "
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoA} className=" mx-1 flex-grow-1 " />
             </div>
           </div>
         </Carousel.Item>
@@ -90,14 +70,10 @@ const Top = () => {
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold pe-2 mb-auto flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoB}
-                className=" mx-1 flex-grow-1 "
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoB} className=" mx-1 flex-grow-1 " />
             </div>
           </div>
         </Carousel.Item>
@@ -106,14 +82,10 @@ const Top = () => {
             <div className="d-flex flex-column">
               <p className="text-white mb-1">Nuova stazione radio</p>
               <p className="text-white fw-bold pe-2 mb-auto flex-grow-1">
-                Rilassati, al rest pensiamo noi, ascolta Apple Music Chill
+                Rilassati, al resto pensiamo noi, ascolta Apple Music Chill
               </p>
 
-              <img
-                src={unoC}
-                className=" mx-1 flex-grow-1 "
-                style={{ width: "98%", objectFit: "contain" }}
-              />
+              <img src={unoC} className=" mx-1 flex-grow-1 " />
             </div>
           </div>
         </Carousel.Item>
